ecfdb7acdb878959bc1b66328cf8cda9f108b20d
DevRussia <87311012+DevRussia@users.noreply.github.com>
2021-07-16 12:22:43 +0900

    게임 출처에 대한 소스 추가

--- stockbot.js
+++ stockbot.js
@@ -2,6 +2,7 @@ const scriptName = "stockbot";
 
 function response(room, msg, sender, isGroupChat, replier, imageDB, packageName) {
     //simpcode and 베타맥스
+    const COMPRESS = "\u200b".repeat(500);
     const profile = java.lang.String(imageDB.getProfileImage()).hashCode();
     const prefix = "/"//명령어 접두사.
     var Command = function(ctx){
@@ -26,6 +27,8 @@ function response(room, msg, sender, isGroupChat, replier, imageDB, packageName)
         userdata: {}
     }
     function StockGame(){
+        this.version="v0.1-alpha"
+        this.developers=["DevRussia","betamax"];
         this.path="sdcard/StockBot/StockData.json";
         this.userpath="sdcard/StockBot/users/"+profile+" ("+sender+").txt"
         this.userfile=JSON.parse(FileStream.read(this.userpath))
@@ -112,7 +115,7 @@ function response(room, msg, sender, isGroupChat, replier, imageDB, packageName)
         if(FileStream.read(this.userpath)!==null){
             return true
         }else{
-            return "가입해주세요"
+            return "가입을 하지 않아 주식 게임을 이용할수 없습니다. 가입을 먼저해주세요.\n"+prefix+"주식 가입 을 채팅창에 입력해보세요"
         }
     }
 
@@ -147,8 +150,17 @@ function response(room, msg, sender, isGroupChat, replier, imageDB, packageName)
             }
         }else if(cmd[1]=="지갑"||cmd[1]=="ㅈㄱ"||cmd[1]=="돈"){
             replier.reply(room,sender+"님이 보유하고 있는 코인은 "+Game.MyMoney()+"입니다.")
+            /**
+             * 개발자 본인이 아닌 경우 정보, 도움말, 명령어, 명령어에 있는 소스 출처를 건드려서는 안됩니다. 
+             * 건드리거나 개발자 본인을 사칭하는 경우 불이익이나 법적 처벌을 받을수 있습니다.
+             */
+        }else if(cmd[1]=="정보"){
+            replier.reply("해당 게임에 대한 정보입니다!"+COMPRESS+"\n\n제작자: DevRussia&BetaMax\n\n해당 게임은 다음 링크에서 다운받을수 있습니다.\nhttps://github.com/DevRussia/KakaoTalk-stockgame")
+        }else if(cmd[1]=="도움말"||cmd[1]=="명령어"){
+            replier.reply(room,"도움말입니다"+COMPRESS+"\n\n"+prefix+"주식 가입 - 주식에 가입합니다\n"+prefix+"주식 매수 <회사명> <구매할 주 수>- 해당 회사명의 주식을 구매할 주 수만큼 구매합니다\n\n<>(꺽쇠)는 진짜 입력하는것이 아닙니다. 다음 예와 같이 입력해주세요! (예: "+prefix+"주식 매수 betamax 1)")
+            replier.reply("Made BY DevRussia & betamax")
         }else{
-            replier.reply("없는 명령어입니다.")
+            replier.reply(prefix+"주식 도움말 혹은 "+prefix+"주식 명령어를 입력해보세요.")
         }
     }
 }
